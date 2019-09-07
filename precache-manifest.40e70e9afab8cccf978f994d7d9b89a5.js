adbbb06b3c1c81bce4cdd0f30bc224844198057e
avinash <avinashp@moofwd.com>
2019-09-07 15:42:18 +0530

    Updates

---
+++ precache-manifest.40e70e9afab8cccf978f994d7d9b89a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76faa73bf7ad325705922d660caddb20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5971d18af7f3416ee4d3",
+    "url": "/static/css/main.62a9c437.chunk.css"
+  },
+  {
+    "revision": "9a2291807d3fd572a7de",
+    "url": "/static/js/2.0fbdf460.chunk.js"
+  },
+  {
+    "revision": "5971d18af7f3416ee4d3",
+    "url": "/static/js/main.fc73e0b8.chunk.js"
+  },
+  {
+    "revision": "4928dde68214cc50a4c6",
+    "url": "/static/js/runtime~main.13de7fff.js"
+  }
+]);
